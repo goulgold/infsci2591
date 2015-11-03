251b389014d7d96dc584773832cef85721e16082
Ubuntu <ubuntu@ip-172-31-8-226.ec2.internal>
2015-11-03 05:01:16 +0000

    proj3 ex2

---
+++ proj3/ex2.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+class Knapsack {
+    private:
+        static int *p_;
+        static int *w_;
+        static int n_;
+        static int W_;
+
+        struct node {
+            int weight;
+            int profit;
+            int level;
+        };
+
+        static double bound(node n) {
+            int totweight = n.weight;
+            double result = n.profit;
+            int j = n.level + 1;
+            while (j <= n_ && totweight + w_[j] <= W_) {
+                 totweight += w_[j];
+                 result += p_[j];
+                 j++;
+            }
+            if (j <=n_ ) {
+                 result += (W_ - n.weight) * p_[j]/w_[j];
+            }
+            return result;
+        }
+
+    public:
+        static void knapsack (int n,
+                              int p[],
+                              int w[],
+                       int W,
+                       int& maxprofit) {
+            n_ = n;
+            p_ = p;
+            w_ = w;
+            W_ = W;
+            queue<node> Q;
+            node v, u;
+            maxprofit = 0;
+            v.weight = 0;
+            v.profit = 0;
+            v.level = 0;
+            Q.push(v);
+            while (!Q.empty()) {
+                v = Q.front();
+                Q.pop();
+                u.level = v.level + 1;
+                if (u.level <= n_) {
+                    u.weight = v.weight + w[u.level];
+                    u.profit = v.profit + p[u.level];
+                    if (u.profit > maxprofit && u.weight < W) {
+                        maxprofit = u.profit;
+                    }
+                    if (bound(u) > maxprofit) {
+                        Q.push(u);
+                    }
+                    u.weight = v.weight;
+                    u.profit = v.profit;
+                    if (bound(u) > maxprofit) {
+                        Q.push(u);
+                    }
+                }
+            }
+        }
+};
+
+int *Knapsack::p_;
+int *Knapsack::w_;
+int Knapsack::n_;
+int Knapsack::W_;
+
+int main(int argc, char* argv[]) {
+    int level = 4;
+    int p_[] = {0, 40, 30, 50, 10};
+    int w_[] = {0, 2, 5, 10, 5};
+    int result;
+    Knapsack::knapsack(level, p_, w_, 16, result);
+    cout << result << endl;
+    return 0;
+}
